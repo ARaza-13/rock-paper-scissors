51606ab725828ddecd4533d90f99818443148e9f
Ali Raza <aliraza021419@gmail.com>
2023-03-15 01:24:51 -0400

    Added a game function to play 5 rounds and keeps score, and cleaned up the playRound function

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 // computer randomly picks rock, paper, or scissors //
 function getComputerChoice() {
@@ -8,30 +10,36 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-// function that plays a single round of rock, paper, scissors //
+// function that plays a single round of rock, paper, scissors and keeps score//
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     let winMsg = `You win! ${playerSelection} beats ${computerSelection}`;
     let loseMsg = `You lose, ${computerSelection} beats ${playerSelection}`;
     let tieMsg = "It's a tie";
 
-    if (playerSelection == "rock" && computerSelection == "scissors") {
+    if ((playerSelection == "rock" && computerSelection == "scissors") || 
+        (playerSelection == "paper" && computerSelection == "rock") || 
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+        playerScore++;
         return winMsg;
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        return winMsg;
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return winMsg;
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        return loseMsg;
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return loseMsg;
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return loseMsg;
-    } else {
+    } else if (playerSelection == computerSelection) {
         return tieMsg;
+    } else {
+        computerScore++;
+        return loseMsg;
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// function that plays 5 rounds and shows the score//
+function game() {
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Enter: 'rock', 'paper' or 'scissors'");
+        let computerSelection = getComputerChoice();
+
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("The score is: Player = " + playerScore + " | Computer = " + computerScore);
+    }
+}   
+
+console.log(game());
